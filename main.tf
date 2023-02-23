e0004f601bd74bf0ba105de744d51915a17bb981
Aarti Gupta <gupta.aart@northeastern.edu>
2023-02-23 16:01:02 -0500

    Added configuration for ec2 instance and security group.

--- main.tf
+++ main.tf
@@ -1,45 +1,45 @@
-module vpc {
-    source = "./network-config/vpc"
-    vpc_name_tag = var.vpc_name_tag
-    vpc_cidr_block = var.vpc_cidr_block
+module "vpc" {
+  source         = "./network-config/vpc"
+  vpc_name_tag   = var.vpc_name_tag
+  vpc_cidr_block = var.vpc_cidr_block
 }
 
 module "private_subnet" {
-  source = "./network-config/private-subnet"
-  private_subnet_cidr_blocks = var.private_subnet_cidr_blocks
-  vpc_id = module.vpc.vpc_id
-  private_subnet_name_tag = var.private_subnet_name_tag
-  availability_zones = var.availability_zones
+  source                     = "./network-config/private-subnet"
+  private_subnet_cidr_blocks = local.private_subnet_cidr_blocks
+  vpc_id                     = module.vpc.vpc_id
+  private_subnet_name_tag    = var.private_subnet_name_tag
+  availability_zones         = local.availability_zones
 }
 
 module "public_subnet" {
-  source = "./network-config/public-subnet"
-  public_subnet_cidr_blocks = var.public_subnet_cidr_blocks
-  vpc_id = module.vpc.vpc_id
-  public_subnet_name_tag = var.public_subnet_name_tag
-  availability_zones = var.availability_zones
+  source                    = "./network-config/public-subnet"
+  public_subnet_cidr_blocks = local.public_subnet_cidr_blocks
+  vpc_id                    = module.vpc.vpc_id
+  public_subnet_name_tag    = var.public_subnet_name_tag
+  availability_zones        = local.availability_zones
 }
 
 module "igw" {
-  source = "./network-config/igw"
-  vpc_id = module.vpc.vpc_id
+  source  = "./network-config/igw"
+  vpc_id  = module.vpc.vpc_id
   igw_tag = var.IGW_tag
 }
 
 module "public_route_table" {
-  source = "./network-config/public-rt"
-  public_route_cidr = var.public_route_cidr
-  rt_tag_public = var.rt_tag_public
-  public_subnet_cidr_blocks = var.public_subnet_cidr_blocks
-  vpc_id = module.vpc.vpc_id
-  igw_id = module.igw.igw_id
-  public_subnets = module.public_subnet.public_subnets
+  source                    = "./network-config/public-rt"
+  public_route_cidr         = var.public_route_cidr
+  rt_tag_public             = var.rt_tag_public
+  public_subnet_cidr_blocks = local.public_subnet_cidr_blocks
+  vpc_id                    = module.vpc.vpc_id
+  igw_id                    = module.igw.igw_id
+  public_subnets            = module.public_subnet.public_subnets
 }
 
 module "private_route_table" {
-  source = "./network-config/private-rt"
-  private_subnet_cidr_blocks = var.private_subnet_cidr_blocks
-  rt_tag_private = var.rt_tag_private
-  vpc_id = module.vpc.vpc_id
-  private_subnets = module.private_subnet.private_subnets
+  source                     = "./network-config/private-rt"
+  private_subnet_cidr_blocks = local.private_subnet_cidr_blocks
+  rt_tag_private             = var.rt_tag_private
+  vpc_id                     = module.vpc.vpc_id
+  private_subnets            = module.private_subnet.private_subnets
 }
