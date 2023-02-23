61c58590cd83e46d105942183e86ec5b48e310b4
Aarti Gupta <gupta.aart@northeastern.edu>
2023-02-23 16:18:44 -0500

    Formatting done.

--- network-config/igw/igw.tf
+++ network-config/igw/igw.tf
@@ -1,12 +1,12 @@
-variable vpc_id {}
-variable igw_tag {}
+variable "vpc_id" {}
+variable "igw_tag" {}
 
 resource "aws_internet_gateway" "IGW" {
-    vpc_id = var.vpc_id
-    
-    tags = {
-        Name = var.igw_tag
-    }
+  vpc_id = var.vpc_id
+
+  tags = {
+    Name = var.igw_tag
+  }
 }
 
-output "igw_id" { value = "${aws_internet_gateway.IGW.id}" }
+output "igw_id" { value = aws_internet_gateway.IGW.id }
--- network-config/private-rt/private-rt.tf
+++ network-config/private-rt/private-rt.tf
@@ -1,18 +1,18 @@
-variable vpc_id {}
-variable rt_tag_private {}
-variable private_subnets {}
-variable private_subnet_cidr_blocks {}
+variable "vpc_id" {}
+variable "rt_tag_private" {}
+variable "private_subnets" {}
+variable "private_subnet_cidr_blocks" {}
 
 resource "aws_route_table" "private_route_table" {
-    vpc_id = var.vpc_id
+  vpc_id = var.vpc_id
 
-    tags = {
-        Name = var.rt_tag_private
-    }
+  tags = {
+    Name = var.rt_tag_private
+  }
 }
 
 resource "aws_route_table_association" "rt_associate_private" {
-    count = length(var.private_subnet_cidr_blocks)
-    subnet_id = element(var.private_subnets[*].id, count.index)
-    route_table_id = aws_route_table.private_route_table.id
+  count          = length(var.private_subnet_cidr_blocks)
+  subnet_id      = element(var.private_subnets[*].id, count.index)
+  route_table_id = aws_route_table.private_route_table.id
 }
